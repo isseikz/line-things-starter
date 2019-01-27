40fb5db490313f16ae77250b3fce2deeac9bfdf4
isseikz <kuzumaki-issei-sk@ynu.jp>
2019-01-27 20:02:38 +0900

    updated uuid

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'df9b6004-19ae-425c-abc8-59ed5317d2af'; // LED, Button
+const USER_SERVICE_UUID         = 'aaadfb25-d3f8-4290-b143-c9dc9a141a08'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
